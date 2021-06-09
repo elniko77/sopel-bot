82196e2469954bcf62f95ee084ce5933b9ab1206
Nico Sosa (nss) <nico77@gmail.com>
2021-06-09 22:52:33 +0000

    Formateado dolar y muestra todas las variantes

--- sopel_modules/dolar.py
+++ sopel_modules/dolar.py
@@ -9,14 +9,38 @@ import json
 
 @module.commands('dolar')
 def dolar(bot, trigger):
-    data = requests.get('http://api.bluelytics.com.ar/v2/latest')
 
-    if data.status_code == 200:
-        jdata = data.json()
-            
-        oficial = jdata["oficial"]["value_avg"]
-        blue = jdata["blue"]["value_avg"]
-        dolarprice = 'Oficial: ' + str(oficial) + ' / Blue: ' + str(blue)
-        bot.say(dolarprice)
+    cuantos = trigger.group(2)
+
+    if not cuantos:
+        cuantos = float(1)
     else:
-        return 'Problemas con la api'
+        cuantos = float(cuantos)
+          
+
+    api_url = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales'
+    tipodolar = ["Dolar Oficial", "Dolar Blue", "Dolar Bolsa", "Dolar Contado con Liqui"]
+    
+    try:
+        r = requests.get(api_url)
+    except:
+        raise Exception("Error obteniendo cotización del dolar... ‾_(ツ)_/‾")
+    
+    data = r.json()
+    mensaje = ""
+    if r.status_code != 200:
+        raise Exception('Error: {}'.format(data['message']))
+    else:
+        for cotizacion in data:
+            if (cotizacion['casa']['nombre'] in tipodolar):
+                compra = float(cotizacion['casa']['compra'].replace(',', '.')) * cuantos
+                venta = float(cotizacion['casa']['venta'].replace(',', '.')) * cuantos
+
+                mensaje = mensaje + f"[{cotizacion['casa']['nombre']}] {compra:.2f} - {venta:.2f} ::  " 
+                
+            if(cotizacion['casa']['nombre'] == 'Dolar Oficial'):
+                    #solidario = venta * 1.65 * $howmuch if $type eq 'Dolar Oficial';
+                    solidario = float(venta) * 1.65
+        
+        mensaje = mensaje + f"[Dolar Solidario ahorro] $ {solidario:.2f}"
+        bot.say(mensaje)
