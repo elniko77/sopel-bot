434add9e24b0bd8d82fbbe050f5729f540f7e522
Nico Sosa (nss) <nico77@gmail.com>
2021-09-28 17:51:26 +0000

    plugin goldill be ignored, and an empty message aborts the commit.

--- sopel_modules/crypto.py
+++ sopel_modules/crypto.py
@@ -11,6 +11,7 @@ import re
 def crypto(bot, trigger):
 
     api_bitso = 'https://api.bitso.com/v3/ticker/?book='
+    api_coingecko = 'https://api.coingecko.com/api/v3/coins/cardano?localization=false&community_data=false&developer_data=false&sparkline=false'
     
     monedas = { 'Ripple': 'xrp_usd', 'Ethereum' : 'eth_usd', 'Dai(ars)' : 'dai_ars' }
 
@@ -31,5 +32,18 @@ def crypto(bot, trigger):
         else:
             if (data['success']):
                 mensaje = mensaje + f"[{token}] {data['payload']['bid']} "
-    
+
+    try:
+        r = requests.get(api_coingecko)
+    except:
+        raise Exception("Error obteniendo cotización... ‾_(ツ)_/‾")
+
+    data = r.json()
+
+    if r.status_code != 200:
+        raise Exception('Error: {}'.format(data['message']))
+    else:
+        mensaje = mensaje + f"[Cardano] {data['market_data']['current_price']['usd']}"
+
+
     bot.say(mensaje)
