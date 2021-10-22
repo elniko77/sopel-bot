9031e61cb71b622fc4ca9340080a3ccd82050ea3
Nico Sosa (nss) <nico77@gmail.com>
2021-10-22 16:17:28 +0000

    Btc en .crypto

--- sopel_modules/crypto.py
+++ sopel_modules/crypto.py
@@ -12,7 +12,8 @@ def crypto(bot, trigger):
 
     api_bitso = 'https://api.bitso.com/v3/ticker/?book='
     api_coingecko = 'https://api.coingecko.com/api/v3/coins/cardano?localization=false&community_data=false&developer_data=false&sparkline=false'
-    
+    api_alpha =  'https://api.coingecko.com/api/v3/coins/alpha-finance?localization=false&community_data=false&developer_data=false&sparkline=false'
+
     monedas = { 'Ripple': 'xrp_usd', 'Ethereum' : 'eth_usd', 'Dai(ars)' : 'dai_ars' }
 
     mensaje = ""
@@ -43,7 +44,34 @@ def crypto(bot, trigger):
     if r.status_code != 200:
         raise Exception('Error: {}'.format(data['message']))
     else:
-        mensaje = mensaje + f"[Cardano] {data['market_data']['current_price']['usd']}"
+        mensaje = mensaje + f"[Cardano] {data['market_data']['current_price']['usd']} "
+
+
+    try:
+        r = requests.get(api_alpha)
+    except:
+        raise Exception("Error obteniendo cotización... ‾_(ツ)_/‾")
+
+    data = r.json()
+
+    if r.status_code != 200:
+        raise Exception('Error: {}'.format(data['message']))
+    else:
+        mensaje = mensaje + f"[Alpha] {data['market_data']['current_price']['usd']}"
+
+
+    api_coinbase = 'https://api.coinbase.com/v2/prices/spot?currency=USD'
+
+    try:
+        r = requests.get(api_coinbase)
+    except:
+        raise Exception("Error obteniendo cotización del btc... ‾_(ツ)_/‾")
+    data = r.json()
+
+    if r.status_code != 200:
+        raise Exception('Error: {}'.format(data['message']))
+    else:
+        mensaje = mensaje + f" [Btc] {float(data['data']['amount']):.2f}"
 
 
     bot.say(mensaje)
