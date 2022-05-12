d3d9b3bb596820626bcccd01bf0b893bd855c0b2
Nico Sosa (nss) <nico77@gmail.com>
2022-05-12 12:18:45 +0000

    Cambiado alpha por matic

--- sopel_modules/crypto.py
+++ sopel_modules/crypto.py
@@ -13,6 +13,7 @@ def crypto(bot, trigger):
     api_bitso = 'https://api.bitso.com/v3/ticker/?book='
     api_coingecko = 'https://api.coingecko.com/api/v3/coins/cardano?localization=false&community_data=false&developer_data=false&sparkline=false'
     api_alpha =  'https://api.coingecko.com/api/v3/coins/alpha-finance?localization=false&community_data=false&developer_data=false&sparkline=false'
+    api_matic =  'https://api.coingecko.com/api/v3/coins/matic-network?localization=false&community_data=false&developer_data=false&sparkline=false'
 
     monedas = { 'Ripple': 'xrp_usd', 'Ethereum' : 'eth_usd', 'Dai(ars)' : 'dai_ars' }
 
@@ -47,8 +48,20 @@ def crypto(bot, trigger):
         mensaje = mensaje + f"[Cardano] {data['market_data']['current_price']['usd']} "
 
 
+#    try:
+#        r = requests.get(api_alpha)
+#    except:
+#        raise Exception("Error obteniendo cotización... ‾_(ツ)_/‾")
+#
+#    data = r.json()
+#
+#    if r.status_code != 200:
+#        raise Exception('Error: {}'.format(data['message']))
+#    else:
+#        mensaje = mensaje + f"[Alpha] {data['market_data']['current_price']['usd']}"
+
     try:
-        r = requests.get(api_alpha)
+        r = requests.get(api_matic)
     except:
         raise Exception("Error obteniendo cotización... ‾_(ツ)_/‾")
 
@@ -57,7 +70,7 @@ def crypto(bot, trigger):
     if r.status_code != 200:
         raise Exception('Error: {}'.format(data['message']))
     else:
-        mensaje = mensaje + f"[Alpha] {data['market_data']['current_price']['usd']}"
+        mensaje = mensaje + f"[Matic] {data['market_data']['current_price']['usd']}"
 
 
     api_coinbase = 'https://api.coinbase.com/v2/prices/spot?currency=USD'
